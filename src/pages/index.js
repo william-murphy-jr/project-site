5261f46a2d42639f6583974709e2673b057f16a9
Bill Murphy <bill559m@gmail.com>
2019-03-02 16:10:56 -0800

    init portfolio page with "Hello Mom"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
+import appCss from "./app.module.css"
+// export default () => <div>Hello Cruel world!</div>
 
-export default () => <div>Hello world!</div>
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className={appCss.test}>Hello there Mom Love Ya!</div>
+    )
+  }
+};
+// const App = () => {
+//   return (
+//     <div className={appCss.test}>Hello Mom</div>
+//   )
+// };
+
+export default App;
